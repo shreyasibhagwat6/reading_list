afa0625b57959d4333ac07ef0440f605eb7015b2
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-02-04 15:10:59 -0500

    bookCreate component functionality and styling completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App () {
     const [books, setBooks] = useState([]);
 
+    const createBook = (title) => {
+        console.log('need to add book with title', title);
+    }
+
     return (
-        <div>App</div>
+        <div>
+            <BookCreate onCreate={createBook} />
+        </div>
     )
 }
 
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,29 @@
-function BookCreate () {
+import { useState } from "react";
+
+function BookCreate ({ onCreate }) {
+    const [title, setTitle] = useState('');
+
+    // this event handler takes value in input and updates the state
+    const handleChange = (event) => {
+      setTitle(event.target.value);
+    }
+
+    // this event handler takes the title and runs onCreate once form is submitted
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
+
     return (
-        <div>BookCreate</div>
+        <div className="book-create">
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange}></input>
+                <button className="button">Submit</button>
+            </form>
+        </div>
     )
 }
 
