9b5c0c860cb473106bcf65c22cd7161a555e0732
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-02-05 08:09:45 -0500

    functionality of adding and showing books completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App () {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
         console.log('need to add book with title', title);
+        const updatedBooks = [...books, 
+            { id:Math.round(Math.random()*999), 
+                title,
+            },
+        ];
+        setBooks(updatedBooks);
     }
+    console.log(books)
 
     return (
-        <div>
+        <div className="app">
+            <BookList books={books}/>
             <BookCreate onCreate={createBook} />
         </div>
     )
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,14 @@
-function BookList () {
+import BookShow from "./BookShow";
+
+function BookList ({ books }) {
+    const renderBooks = books.map((book) => {
+        return (
+            <BookShow key={book.id} book={book} />
+        )
+    })
+
     return (
-        <div>BookList</div>
+        <div className="book-list">{renderBooks}</div>
     )
 }
 
