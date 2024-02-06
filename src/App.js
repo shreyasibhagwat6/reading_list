6d021bdfad612f66db25327d0c22d2ba1ccd2021
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-02-06 11:38:33 -0500

    local db working and making get, put and delete requests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App () {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books')
+
+        setBooks(response.data);
+    };
+
+    useEffect(()=> {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async(id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+
+        console.log(response);
+
         const updatedBooks = books.map((book) => {
            if (book.id === id) {
-            return { ...book, title: newTitle};
+            return { ...book, ...response.data};
            } else {
             return book;
            };
@@ -17,23 +34,25 @@ function App () {
         setBooks(updatedBooks);
     };
     
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        const response = await axios.delete(`http://localhost:3001/books/${id}`)
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         })
         setBooks(updatedBooks);
     }
     
-    const createBook = (title) => {
-        console.log('need to add book with title', title);
-        const updatedBooks = [...books, 
-            { id:Math.round(Math.random()*999), 
-                title,
-            },
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title: title
+        })
+        
+        const updatedBooks = [
+            ...books, response.data
         ];
         setBooks(updatedBooks);
     }
-    console.log(books)
 
     return (
         <div className="app">
